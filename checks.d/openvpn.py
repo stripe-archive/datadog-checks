de195016182f9a1c0ff7421f31ede73ad7682bac
Aditya Mukerjee <dev@chimeracoder.net>
2016-11-01 12:30:59 -0400

    Emit service checks from OpenVPN check

--- checks.d/openvpn.py
+++ checks.d/openvpn.py
@@ -1,23 +1,35 @@
 import csv
+import os
+import datetime
 from collections import defaultdict
 
 # project
 from checks import AgentCheck
 
+# 15 seconds appears to be too short of an interval
+FILE_REFRESH_SECONDS = 25
 
-# Each user really should only have one active connection
-# but empirically, a user can have 3 connections within a 10-second
-# interval without it being the sign of a problem (probably 
-# due to reconnections)
-USER_MAX_CONNECTIONS = 4
-
-class Splunk(AgentCheck):
+class OpenVPN(AgentCheck):
     def check(self, instance):
+        # path and vpn_name should always be present
         filename = instance['path']
-        self.parse_status_file(filename)
+        vpn_name = instance['vpn_name']
+        instance_tags = instance['tags']
+        self.parse_status_file(filename, instance_tags)
 
+        if not os.path.isfile(filename):
+            self.service_check('openvpn.status.is_running', AgentCheck.CRITICAL,
+                    message='VPN level {0} is not running'.format(vpn_name),
+                    tags = instance_tags)
 
-    def parse_status_file(self, filename):
+        mtimestamp = os.path.getmtime(filename)
+        now = int(datetime.datetime.now().strftime("%s"))
+        if now - mtimestamp > FILE_REFRESH_SECONDS:
+            self.service_check('openvpn.status.is_running', AgentCheck.CRITICAL,
+                    message='VPN level {0} is not running'.format(vpn_name),
+                    tags = instance_tags)
+
+    def parse_status_file(self, filename, instance_tags):
         with open(filename) as csvfile:
             reader = csv.reader(csvfile, delimiter='\t')
             users = defaultdict(int)
@@ -31,12 +43,6 @@ class Splunk(AgentCheck):
                 users[username] += 1
 
             for user, num in users.iteritems():
-                if num > USER_MAX_CONNECTIONS:
-                    raise Exception('User {0} connected too many times: {1}'.format(user, str(num)))
-
-
-
-
-
+                self.gauge('openvpn.users.connections', num, tags=instance_tags + ['common_name:{0}'.format(user)])
 
 

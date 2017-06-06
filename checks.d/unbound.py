0045b30f907492f6fbc0fd53b0e0e2a2e6c7a9dd
Paul Carleton <carleton@stripe.com>
2017-06-06 11:14:46 -0700

    Add check to capture stats from unbound dns resolver (#59)
    * Add check for getting stats from unbound
    
    * Add categories of metrics
    
    * Add example unbound conf file
    
    * Fix tag names for suffix tags
    
    * Add sudo option to config file and separate total metric
    
    * Add comment explaining sudo

---
+++ checks.d/unbound.py
@@ -0,0 +1,114 @@
+import subprocess
+
+from checks import AgentCheck
+
+
+class UnboundCheck(AgentCheck):
+    SERVICE_CHECK_NAME = 'unbound'
+
+    def get_cmd(self):
+        if self.init_config.get('sudo'):
+            cmd = 'sudo unbound-control stats'
+        else:
+            cmd = 'unbound-control stats'
+
+        return cmd
+
+    def get_stats(self):
+        cmd = self.get_cmd()
+
+        try:
+            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as e:
+            error_msg = 'ERROR CALLING {0}: {1}  {2}'.format(cmd, e, e.output)
+            self.service_check(
+                self.SERVICE_CHECK_NAME,
+                AgentCheck.CRITICAL,
+                message=error_msg,
+            )
+            self.log.error()
+            return None
+
+        return output
+
+    def parse_stat(self, stat):
+        label, stat = stat.split("=")
+        prefix, suffix = label.split(".", 1)
+
+        by_tag_labels = [
+            'num.query.flags',
+            'num.query.edns',
+            'num.answer.rcode',
+        ]
+
+        tags = []
+        if prefix.startswith('thread'):
+            tags.append("thread:{}".format(prefix[-1]))
+            metric = suffix
+        elif any(label.startswith(lbl) for lbl in by_tag_labels):
+            # E.g.
+            # num.query.flags.QR
+            # metric = num.query.flags
+            # tag = {flags: QR}
+            metric, tag = label.rsplit('.', 1)
+            _, tag_name = metric.rsplit('.', 1)
+            tags.append("{}:{}".format(tag_name, tag))
+        else:
+            metric = label
+
+
+        rate_metrics = [
+            "num.queries",
+            "num.cachehits",
+            "num.cachemiss",
+            "num.prefetch",
+            "num.recursivereplies",
+            "requestlist.overwritten",
+            "requestlist.exceeded",
+            "tcpusage",
+            "num.query.flags",
+            "num.query.edns",
+            "num.answer.rcode",
+        ]
+        gauge_metrics = [
+            "requestlist.max",
+            "requestlist.avg",
+            "requestlist.overwritten",
+            "requestlist.current.all",
+            "requestlist.current.user",
+            "recursion.time.avg",
+            "recursion.time.median",
+        ]
+
+        rate_metrics = rate_metrics + ["total.{}".format(m) for m in rate_metrics]
+        gauge_metrics= gauge_metrics + ["total.{}".format(m) for m in gauge_metrics]
+
+        ns_metric = "unbound.{}".format(metric)
+        if metric in rate_metrics:
+            self.rate(ns_metric, float(stat), tags)
+        elif metric in gauge_metrics:
+            self.gauge(ns_metric, float(stat), tags)
+        else:
+            self.count(ns_metric, float(stat), tags)
+
+    def check(self, instance):
+        stats = self.get_stats()
+
+        if stats is None:
+            return
+
+        try:
+            for line in stats.split("\n"):
+                if not line:
+                    continue
+                self.parse_stat(line)
+        except Exception as e:
+            self.service_check(
+                self.SERVICE_CHECK_NAME,
+                AgentCheck.CRITICAL,
+                message="Error when parsing unbound stats: {0}".format(e),
+            )
+            return
+
+        self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK)
+
---
+++ tests/checks/integration/test_unbound.py
@@ -0,0 +1,73 @@
+
+# project
+from os import path
+
+from checks import AgentCheck
+from tests.checks.common import AgentCheckTest, load_check
+
+class TestFileUnit(AgentCheckTest):
+    CHECK_NAME = 'unbound'
+
+    FIXTURE_PATH = path.join(
+        path.dirname(path.realpath(__file__)),
+        'fixtures',
+        'unbound'
+    )
+
+    def test_check_exists(self):
+        conf = {}
+        load_check('unbound', conf, {})
+
+    def test_output(self):
+        conf = {
+            'init_config': {},
+            'instances': [
+                {}
+            ]
+        }
+        filename = path.join(self.FIXTURE_PATH, 'stats.txt')
+
+        def get_stats():
+            with open(filename, "r") as fh:
+                return fh.read()
+
+        # Run twice to establish rates.
+        self.run_check_twice(conf, mocks={'get_stats': get_stats})
+
+        self.assertMetric("unbound.num.queries", value=0, tags=['thread:0'])
+        self.assertMetric("unbound.total.requestlist.max", value=1212)
+        self.assertMetric("unbound.requestlist.max", value=1079, tags=['thread:2'])
+        self.assertMetric("unbound.num.query.flags", value=0, tags=['flags:RD'])
+        self.assertMetric("unbound.num.query.edns", value=0, tags=['edns:present'])
+        self.assertMetric("unbound.num.answer.rcode", value=0, tags=['rcode:NOERROR'])
+        self.assertServiceCheck('unbound', AgentCheck.OK)
+
+    def test_output_failure(self):
+        conf = {
+            'init_config': {},
+            'instances': [
+                {}
+            ]
+        }
+        def get_stats():
+            return "garbageoutput"
+
+        self.run_check(conf, mocks={'get_stats': get_stats})
+
+        self.assertServiceCheck('unbound', AgentCheck.CRITICAL)
+
+    def check_sudo(self, sudo_value):
+        conf = {
+            'init_config': {"sudo": sudo_value},
+            'instances': [
+                {}
+            ]
+        }
+
+        check = load_check('unbound', conf, {})
+        cmd = check.get_cmd()
+        self.assertEqual(cmd.startswith("sudo "), sudo_value)
+
+    def test_sudo(self):
+        self.check_sudo(True)
+        self.check_sudo(False)

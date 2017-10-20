296f1be2c5fb878550cc36c5285056d8d8ca8826
Aditya Mukerjee <aditya@stripe.com>
2017-10-20 12:55:47 -0400

    Track error executing check

--- checks.d/veneur.py
+++ checks.d/veneur.py
@@ -9,6 +9,7 @@ class Veneur(AgentCheck):
 
     VERSION_METRIC_NAME = 'veneur.deployed_version'
     BUILDAGE_METRIC_NAME = 'veneur.build_age'
+    ERROR_METRIC_NAME = 'veneur.agent_check.errors_total'
 
     def check(self, instance):
         success = 0
@@ -24,10 +25,11 @@ class Veneur(AgentCheck):
             builddate = datetime.datetime.fromtimestamp(int(r.text))
 
             tdelta = datetime.datetime.now() - builddate
+            self.histogram(self.BUILDAGE_METRIC_NAME, tdelta.seconds)
 
         except:
             success = 0
+            self.increment(self.ERROR_METRIC_NAME)
             raise
         finally:
             self.gauge(self.VERSION_METRIC_NAME, success, tags = ['sha:{0}'.format(sha)])
-            self.histogram(self.BUILDAGE_METRIC_NAME, tdelta.seconds)

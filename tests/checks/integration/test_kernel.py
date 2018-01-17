bc7717bb83dbbaabbf9d1de7cc20025a4b5a5564
Paul Carleton <carleton@stripe.com>
2018-01-16 19:37:33 -0800

    Fix kernel check when no grub default found

--- tests/checks/integration/test_kernel.py
+++ tests/checks/integration/test_kernel.py
@@ -41,3 +41,34 @@ class TestKernelUnit(AgentCheckTest):
                           tags=['kernel:1.2.3.4-generic',
                                 'release:xenial',
                                 'grub_default:1.2.3.3-generic'])
+
+    def test_output_no_grub(self):
+        conf = {
+            'init_config': {},
+            'instances': [
+                {}
+            ]
+        }
+
+        def get_linux_release():
+            return 'trusty'
+
+        def get_kernel_version():
+            return '1.2.3.4-generic'
+
+        def get_grub_menu_lines():
+            return [
+                '# title stuff about grub \n',
+                'no title here\n',
+            ]
+
+        self.run_check(conf, mocks={
+            'get_linux_release': get_linux_release,
+            'get_kernel_version': get_kernel_version,
+            'get_grub_menu_lines': get_grub_menu_lines,
+        })
+
+        self.assertMetric("linux.kernel", value=1, metric_type='gauge',
+                          tags=['kernel:1.2.3.4-generic',
+                                'release:trusty',
+                                'grub_default:unknown'])

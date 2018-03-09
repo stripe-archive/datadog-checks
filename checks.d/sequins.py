b8513010f3b4e132e727b19befe6bd0bef0bf553
Dave Vasilevsky <vasi@stripe.com>
2018-03-09 17:24:55 -0500

    wip

--- checks.d/sequins.py
+++ checks.d/sequins.py
@@ -17,33 +17,46 @@ class Sequins(AgentCheck):
         max_dbs = instance.get('max_dbs', self.DEFAULT_MAX_DBS)
         timeout = self.init_config.get('default_timeout', self.DEFAULT_TIMEOUT)
         resp = self.get_json(instance['url'], timeout)
+        db_names = set(resp['dbs'].keys())
+
+        requests_by_db = {}
+        if 'debug_url' in instance:
+            expvar_url = instance['debug_url'] + '/debug/vars'
+            expvar = self.get_json(expvar_url, timeout)
+            requests_by_db = expvar.get('sequins', {}).get('ByDB', {})
+            db_names |= set(requests_by_db.keys())
 
         self.gauge('sequins.db_count', len(resp['dbs']), tags=instance_tags)
-        for db_name, db in resp['dbs'].iteritems():
+        for db_name in db_names:
             db_tags = instance_tags + ['sequins_db:%s' % db_name]
 
-            num_dbs = len(db['versions'])
-            if num_dbs > max_dbs:
-                raise Exception("%d dbs is more than the configured maximum (%d)" % (num_dbs, max_dbs))
+            if db_name in resp['dbs']:
+                db = resp['dbs'][db_name]
+                num_dbs = len(db['versions'])
+                if num_dbs > max_dbs:
+                    raise Exception("%d dbs is more than the configured maximum (%d)" % (num_dbs, max_dbs))
 
-            self.gauge('sequins.version_count', num_dbs, db_tags)
+                self.gauge('sequins.version_count', num_dbs, db_tags)
 
-            for version_name, version in db['versions'].iteritems():
-                version_tags = db_tags + ['sequins_version:%s' % version_name]
-                self.gauge('sequins.partition_count', version['num_partitions'], version_tags)
-                self.gauge('sequins.missing_partition_count', version['missing_partitions'], version_tags)
-                self.gauge('sequins.underreplicated_partition_count', version['underreplicated_partitions'], version_tags)
-                self.gauge('sequins.overreplicated_partition_count', version['overreplicated_partitions'], version_tags)
-                self.gauge('sequins.average_replication', version['average_replication'], version_tags)
+                for version_name, version in db['versions'].iteritems():
+                    version_tags = db_tags + ['sequins_version:%s' % version_name]
+                    self.gauge('sequins.partition_count', version['num_partitions'], version_tags)
+                    self.gauge('sequins.missing_partition_count', version['missing_partitions'], version_tags)
+                    self.gauge('sequins.underreplicated_partition_count', version['underreplicated_partitions'], version_tags)
+                    self.gauge('sequins.overreplicated_partition_count', version['overreplicated_partitions'], version_tags)
+                    self.gauge('sequins.average_replication', version['average_replication'], version_tags)
 
-                node_counts = {}
-                for node in version['nodes'].itervalues():
-                    st = node['state']
-                    node_counts[st] = node_counts.get(st, 0) + 1
+                    node_counts = {}
+                    for node in version['nodes'].itervalues():
+                        st = node['state']
+                        node_counts[st] = node_counts.get(st, 0) + 1
 
-                for state, count in node_counts.iteritems():
-                    tags = version_tags + ['sequins_node_state:%s' % state.lower()]
-                    self.gauge('sequins.node_count', count, tags)
+                    for state, count in node_counts.iteritems():
+                        tags = version_tags + ['sequins_node_state:%s' % state.lower()]
+                        self.gauge('sequins.node_count', count, tags)
+
+            if db_name in requests_by_db:
+                self.count('sequins.requests_by_db', requests_by_db[db_name] * 60, db_tags)
 
         if 'shard_id' in resp:
             self.gauge('sequins.shard_id', 1, instance_tags + ['sequins_shard:%s' % resp['shard_id']])
